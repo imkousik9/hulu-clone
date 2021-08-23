675ae81d8cda94a142a3804e74dad2ee83fc8793
Kousik Manna <45976284+imkousik9@users.noreply.github.com>
2021-08-23 10:40:42 +0530

    add api key

--- src/requests.js
+++ src/requests.js
@@ -1,3 +1,5 @@
+const API_KEY = process.env.API_KEY;
+
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
